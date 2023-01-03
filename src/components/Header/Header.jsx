afcc7642471ef419a2a0fcf51e20183630b52229
Arsen-Git <asermahambetov@gmail.com>
2023-01-03 15:08:17 +0100

    created Header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,23 @@
+import "./Header.scss";
+
+export default function Header() {
+  return (
+    <header className="container">
+      <div className="header__left">
+        <img width={38} height={38} src="./img/pizza-logo.svg" alt="logo" />
+        <div className="title__container">
+          <h2 className="title">REACT PIZZA</h2>
+          <p className="subtitle">самая вкусная пицца во вселенной</p>
+        </div>
+      </div>
+      <div className="cart">
+        <p className="cart__price">520 ₴</p>
+        <div className="line"></div>
+        <div className="cart__items">
+          <img width={16} height={16} src="./img/cart.svg" alt="cart" />
+          <p className="cart__price">3</p>
+        </div>
+      </div>
+    </header>
+  );
+}
